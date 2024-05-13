dceab0231b1808cec739deb881e039bdf940e178
Enrique Gomez Naar <enriquegomeznaar@gmail.com>
2024-05-13 18:48:59 -0300

    initiation

---
+++ app.js
@@ -0,0 +1,9 @@
+
+  // Connect an event to the html botom 
+  // Event listener for the bottom button click
+document.getElementById("btn").addEventListener("click", handleBottomClick);
+
+// Event handler for the bottom button click
+function handleBottomClick() {
+  console.log("Bottom button clicked");
+}
